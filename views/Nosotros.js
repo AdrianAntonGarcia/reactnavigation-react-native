6cd94bb02353a88b12de5b237702101e043e2b74
adriton <adrtler@gmail.com>
2021-04-05 16:39:36 +0200

    Volver a la pantalla previa

--- views/Nosotros.js
+++ views/Nosotros.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, View, Button} from 'react-native';
 
-export const Nosotros = () => {
-  return <Text>Nosotros</Text>;
+export const Nosotros = ({navigation}) => {
+  const volver = () => {
+    // navigation.navigate('Inicio');
+    // navigation.push('Inicio');
+    navigation.goBack();
+  };
+  return (
+    <View style={styles.contenedor}>
+      <Text>Nosotros</Text>
+      <Button title="Volver" onPress={() => volver()} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Nosotros;
