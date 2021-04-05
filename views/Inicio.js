59ea1420b7229cf209228d808f106ee8aea25e35
adriton <adrtler@gmail.com>
2021-04-05 16:34:34 +0200

    Navegar de una pantalla a otra

--- views/Inicio.js
+++ views/Inicio.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, View, Button} from 'react-native';
 
-export const Inicio = () => {
-  return <Text>Inicio</Text>;
+export const Inicio = ({navigation}) => {
+  const visitarNosotros = () => {
+    navigation.navigate('Nosotros');
+  };
+  return (
+    <View style={styles.contenedor}>
+      <Text>Inicio</Text>
+      <Button title="Ir a nosotros" onPress={() => visitarNosotros()} />
+    </View>
+  );
 };
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Inicio;
